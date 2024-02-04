1d43ef4dde387b08e681eff3b5c4961eb89e405f
ItsThanos1 <157194187+ItsThanos1@users.noreply.github.com>
2024-02-04 16:29:20 -0700

    update3

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var server = express();
 
-server.use(bodyParser.urlencoded({extended: false}));
-server.use(bodyParser.json());
+// Middleware to parse text/plain requests
+server.use(express.text());
 
-server.post("/", function(req,res){
-    console.log(" I got "+ req.body.message);
-    res.send(req.body.message);
+server.post("/", function(req, res) {
+    console.log("I got: " + req.body);
+    res.send(req.body); // Echo back the plain text
 });
 
-server.listen(3000,function(){
-    console.log("express echo server is listening on port 3000");
-})
+server.listen(3000, function() {
+    console.log("Express echo server is listening on port 3000");
+});
